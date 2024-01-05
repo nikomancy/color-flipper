a9fc123c2669f883eff735a12a28227461d437a2
Niko <39569270+nikomancy@users.noreply.github.com>
2024-01-05 10:39:24 -0700

    Able to set background to a random color.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [bgColor, setBgColor] = useState('#ffffff')
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor;
+}, [bgColor])
+
+function changeColor() {
+  const newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  setBgColor(newColor)
+}
+
+
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel='noreferrer'>
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel='noreferrer'>
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={changeColor}>
+          Change Background Color
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
