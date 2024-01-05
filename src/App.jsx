80ef11ea101c6267e05a4b8dbfc666d798482742
Niko <39569270+nikomancy@users.noreply.github.com>
2024-01-05 10:47:23 -0700

    Removed links from react and Vite logos

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,8 @@ function changeColor() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel='noreferrer'>
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel='noreferrer'>
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
